b545d47b639f726c9cfe223f92aeb4c38d3ad502
Andrew Kent <akent@peak6.com>
2022-03-27 10:24:27 -0700

    hacking on hello world

--- src/main/java/org/web3j/Web3App.java
+++ src/main/java/org/web3j/Web3App.java
@@ -6,6 +6,9 @@ import org.web3j.generated.contracts.HelloWorld;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
+
+import java.math.BigInteger;
 
 /**
  * <p>This is the generated class for <code>web3j new helloworld</code></p>
@@ -22,9 +25,20 @@ public class Web3App {
         Credentials credentials = WalletUtils.loadCredentials(walletPassword, walletPath);
         Web3j web3j = Web3j.build(new HttpService(nodeUrl));
         System.out.println("Deploying HelloWorld contract ...");
-        HelloWorld helloWorld = HelloWorld.deploy(web3j, credentials, new DefaultGasProvider(), "Hello Blockchain World!").send();
-        System.out.println("Contract address: " + helloWorld.getContractAddress());
+        // HelloWorld helloWorld = HelloWorld.deploy(web3j, credentials, new DefaultGasProvider(), "Hello Blockchain World!").send();
+        HelloWorld helloWorld = HelloWorld.deploy(web3j, credentials, new GasProvider(), "Hello Blockchain World!").send();
+       System.out.println("Contract address: " + helloWorld.getContractAddress());
         System.out.println("Greeting method result: " + helloWorld.greeting().send());
-       }
-}
+   }
 
+   public static class GasProvider extends StaticGasProvider {
+       // public static final BigInteger GAS_PRICE = BigInteger.valueOf(4_100_000_000L);
+       // public static final BigInteger GAS_LIMIT = BigInteger.valueOf(9_000_000);
+       public static final BigInteger GAS_PRICE = BigInteger.valueOf(4_100_000_000L);
+       public static final BigInteger GAS_LIMIT = BigInteger.valueOf(9_000_000);
+
+       public GasProvider() {
+           super(GAS_PRICE, GAS_LIMIT);
+       }
+   }
+}
