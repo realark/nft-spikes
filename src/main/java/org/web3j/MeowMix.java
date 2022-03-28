eb30ec20380b6513af9abe1b072263bb0ccd6b2c
Andrew Kent <akent@peak6.com>
2022-03-28 15:57:18 -0700

    unpausing works

--- src/main/java/org/web3j/MeowMix.java
+++ src/main/java/org/web3j/MeowMix.java
@@ -3,25 +3,22 @@ package org.web3j;
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
-import org.web3j.generated.contracts.KittyAuction;
-import org.web3j.generated.contracts.KittyCore;
-import org.web3j.generated.contracts.KittyMinting;
-import org.web3j.generated.contracts.SaleClockAuction;
+import org.web3j.generated.contracts.*;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.List;
 
 public class MeowMix {
     private static final Web3App.GasProvider gasProvider = new Web3App.GasProvider();
-    private static final String CONTRACT_ADDRESS = "0xE467Fa0Cbf17B13f709FC26633dD30b10c56b17c";
-    private static final String ADMIN_ADDRESS = "0x30415b1111e0B064930b2D919393EE4Bb741527d";
-    private static final String BUYER_ADDRESS = "0xFC95E480e392258f652049f4a295aF9D4B389cEe";
+    private static Credentials sellerCreds;
     private static Credentials buyerCreds;
 
     static {
         try {
+            sellerCreds = WalletUtils.loadCredentials("passwordpassword", "/home/ark/Downloads/seller");
             buyerCreds = WalletUtils.loadCredentials("passwordpassword", "/home/ark/Downloads/buyer");
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,30 +36,89 @@ public class MeowMix {
         Credentials adminCreds = WalletUtils.loadCredentials(walletPassword, walletPath);
         Web3j web3j = Web3j.build(new HttpService(nodeUrl));
         System.out.println("Deploying CryptoKitties contract ...");
-        // var kittyCore = KittyCore.deploy(web3j, credentials, gasProvider).send();
-        var adminKittyCore = KittyCore.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
+
+        var adminKittyCore = KittyCore.deploy(web3j, adminCreds, gasProvider).send();
+        String CONTRACT_ADDRESS = adminKittyCore.getContractAddress();
+
+        var saleAuction = SaleClockAuction.deploy(web3j, adminCreds, gasProvider, CONTRACT_ADDRESS, BigInteger.valueOf(20)).send();
+        System.out.println("~~~~ saleAuction address: " + saleAuction.getContractAddress());
+        var siringAuction = SiringClockAuction.deploy(web3j, adminCreds, gasProvider, CONTRACT_ADDRESS, BigInteger.valueOf(19)).send();
+        System.out.println("~~~~ saleAuction address: " + siringAuction.getContractAddress());
+        var geneImpl = GeneScience.deploy(web3j, adminCreds, gasProvider, "0x0", CONTRACT_ADDRESS).send();
+        System.out.println("~~~~ deploying sale auction\n");
+        adminKittyCore.setSaleAuctionAddress(saleAuction.getContractAddress()).send();
+        System.out.println("~~~~ deploying siring auction\n");
+        adminKittyCore.setSiringAuctionAddress(siringAuction.getContractAddress()).send();
+        System.out.println("~~~~ deploying gene impl\n");
+        adminKittyCore.setGeneScienceAddress(geneImpl.getContractAddress()).send();
+
+        // var adminKittyCore = KittyCore.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
+
+        var ceoAddress = adminKittyCore.ceoAddress().send();
+        if (!ceoAddress.equals(adminCreds.getAddress())) {
+            throw new RuntimeException("Need the ceo address to equal admin address: \n" + ceoAddress + "\n" + adminCreds.getAddress());
+        }
         // var promoKittyCount = kittyCore.promoCreatedCount().send();
 
-        System.out.println("I have " + adminKittyCore.tokensOfOwner(ADMIN_ADDRESS).send().size() + " cats. Making a new one...");
+        System.out.println("\nShould be good to go. Addresses: " + List.of(adminKittyCore.saleAuction().send(), adminKittyCore.siringAuction().send(), adminKittyCore.geneScience().send()) + "\n");
 
-        var createPromoResult = adminKittyCore.createPromoKitty(BigInteger.ZERO, ADMIN_ADDRESS).send();
+        System.out.println("I have " + adminKittyCore.tokensOfOwner(adminCreds.getAddress()).send().size() + " cats. Making a new one...");
+
+        var createPromoResult = adminKittyCore.createPromoKitty(BigInteger.ZERO, adminCreds.getAddress()).send();
         System.out.println("Made promo kitty. tx hash: " + createPromoResult.getTransactionHash());
-        var myCats = adminKittyCore.tokensOfOwner(ADMIN_ADDRESS).send();
+        var myCats = adminKittyCore.tokensOfOwner(adminCreds.getAddress()).send();
         // System.out.println("I have " + myCats.size() + " cats. Newest one: " + myCats.get(myCats.size() - 1));
-        System.out.println("Here are all my cat ids: " + myCats);
+        System.out.println("Here are all my cat ids: " + myCats + ". Should be of size: " + adminKittyCore.balanceOf(adminCreds.getAddress()).send());
         BigInteger newCatId = (BigInteger) myCats.get(myCats.size() - 1);
 
-        System.out.println("new cat id: " + newCatId + ", owned by: " + adminKittyCore.ownerOf(newCatId).send() + ". I will sell him for a fine price.");
+        System.out.println("\n~~~~~>    sending new cat (" + newCatId + ") to seller (currently owned by " + adminKittyCore.ownerOf(newCatId).send() + ")...\n");
+        System.out.println("\n~~~~~> Sanity check 1. Am I paused? " + adminKittyCore.paused().send() + ". Note that only this address can unpause: " + adminKittyCore.ceoAddress().send() + "\n");
+        /*
+        System.out.println("\n~~~~~>    Pause for fun?\n");
+        adminKittyCore.pause().send();
+        System.out.println("\n~~~~~>    Pause fun done.\n");
+         */
+        /*
+        var adminPausable = Pausable.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
+        adminPausable.unpause().send();
+         */
+        // var adminKittyAccessControl = KittyAccessControl.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
+        // adminKittyAccessControl.unpause().send();
+
+        adminKittyCore.unpause().send();
+
+        // System.out.println("\nis sale auction paused? " + saleAuction.paused().send());
+        // saleAuction.unpause().send();
+
+        System.out.println("\n~~~~~> Sanity check 2. Am I paused? " + adminKittyCore.paused().send() + "\n");
+        System.out.println("\n~~~~~>    Approve Done!\n");
+
+        // var adminOwnership = KittyOwnership.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
+        // adminOwnership.transferFrom(adminCreds.getAddress(), SELLER_ADDRESS, newCatId).send();
+
+        adminKittyCore.transfer(sellerCreds.getAddress(), newCatId).send();
+
+        System.out.println("\n~~~~~>    Done Done!\n");
+
+        var kittyOwner = adminKittyCore.ownerOf(newCatId).send();
+        System.out.println("new cat id: " + newCatId + ", owned by: " + kittyOwner + ". I will sell him for a fine price.");
         { // auction time
-            var sellerAuction = KittyMinting.load(CONTRACT_ADDRESS, web3j, adminCreds, gasProvider);
-            var buyerAuction =  KittyAuction.load(CONTRACT_ADDRESS, web3j, buyerCreds, gasProvider);
-            // System.out.println("\n~~~~~>    Starting auction...\n");
-            // sellerAuction.createSaleAuction(newCatId, BigInteger.valueOf(12), BigInteger.valueOf(20), BigInteger.valueOf(5)).send();
-            // System.out.println("\n~~~~~~>   Auction Started\n");
-            // var bidTx = buyerAuction.bid(newCatId, BigInteger.valueOf(17)).send();
-            // System.out.println("Bid sent. Waiting for auction to end: ");
-            // Thread.sleep(5000);
-            // System.out.println("Sale is over. Cat id: " + newCatId + " is now owned by: " + adminKittyCore.ownerOf(newCatId).send());
+            if (!kittyOwner.equals(sellerCreds.getAddress())) {
+                throw new RuntimeException("Seller doesn't own the kitty.");
+            }
+            /*
+            // var sellerAuction = KittyCore.load(CONTRACT_ADDRESS, web3j, sellerCreds, gasProvider);
+            var sellerAuction = SaleClockAuction.load(saleAuction.getContractAddress(), web3j, sellerCreds, gasProvider);
+            var buyerAuction =  SaleClockAuction.load(saleAuction.getContractAddress(), web3j, buyerCreds, gasProvider);
+            System.out.println("\n~~~~~>    Starting auction...\n");
+            sellerAuction.createAuction(newCatId, BigInteger.valueOf(12), BigInteger.valueOf(20), BigInteger.valueOf(5), sellerCreds.getAddress()).send();
+            System.out.println("\n~~~~~~>   Auction Started\n");
+            var bidTx = buyerAuction.bid(newCatId, BigInteger.valueOf(17)).send();
+            buyerAuction.bid(newCatId, BigInteger.valueOf(17));
+            System.out.println("Bid sent. Waiting for auction to end: ");
+            Thread.sleep(5000);
+            System.out.println("Sale is over. Cat id: " + newCatId + " is now owned by: " + adminKittyCore.ownerOf(newCatId).send());
+             */
         }
     }
 }
